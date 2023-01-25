5c6a629b33e4d9561c287aa558d355e452d6d9be
Daynis Olman <daynis@ditconsulting.com.au>
2023-01-25 14:22:02 +1100

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,27 +1,31 @@
 <?php
-  // Get the input URL
-  $url = $_POST['url'];
-  
-  // Build the API endpoint
-  $api_endpoint = 'https://whatcms.org/API/Tech';
-  $api_key = '';
-  
-  // Use cURL to send a GET request to the API endpoint
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $api_endpoint);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, 'key=' . $api_key . '&url=' . $url);
-  $response = curl_exec($ch);
-  curl_close($ch);
-  
-  // Decode the JSON response
-  $data = json_decode($response);
-  
-  // Extract the necessary information from the results array
-  $name = $data->results[0]->name;
-  $version = $data->results[0]->version;
-  
-  // Return the information as a JSON object
-  echo json_encode(array('name' => $name, 'version' => $version));
-?>
+$url = $_POST['url'];
+$apiKey = '';
+$apiUrl = 'https://whatcms.org/API/Tech';
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "$apiUrl?key=$apiKey&url=$url",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "",
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    $data = json_decode($response);
+    $name = $data->results[0]->name;
+    $version = $data->results[0]->version;
+    echo json_encode(array("name" => $name, "version" => $version));
+}
